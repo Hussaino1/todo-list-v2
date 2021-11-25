818abfda47509b748dc403c285996c932ced03a5
Ubuntu <hussain@dev-jenkins.ofuogsxvp4relmbb2hx3llgxsc.cwx.internal.cloudapp.net>
2021-11-25 19:24:52 +0000

    removing docker install

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 #sudo apt-get install -y curl jq
 
 # install docker 
-curl https://get.docker.com | sudo bash
+#curl https://get.docker.com | sudo bash
 sudo usermod -aG docker jenkins
 
 
